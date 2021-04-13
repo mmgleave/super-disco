eb9efbcaba002d7b4df72d4760ad9aa431b32b81
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-04-13 15:04:19 -0600

    timeblock basic structure and styling

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// header changes
+// append current day to p w ID "currentDay"
+
+
+// time block changes
+// event block background changes to gray if time has passed
+// event block background changes to red if current hour
+// event block background changes to green if future hour
+
+// add event functionality
+// when user clicks on event block, becomes form to input new event
+// when user clicks away from event block, text stays but isn't saved yet
+// when user clicks on save, corresponding event saves to local storage
+// LOCAL STORAGE PERSISTENCE
+
+// user needs to be able to edit an event and save changes not just add new
+
