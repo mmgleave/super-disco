f41b3a5836d9b2f9b6c934e729e157c84b602003
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-04-13 19:49:53 -0600

    add new event text on save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,54 @@
 // header changes
 // append current day to p w ID "currentDay"
+var currentDate = moment().format('dddd MMMM Do YYYY');
+var currentDayEl = $("#currentDay");
+currentDayEl.text(currentDate);
 
+// add the ability to add a task
 
-// time block changes
-// event block background changes to gray if time has passed
-// event block background changes to red if current hour
-// event block background changes to green if future hour
+// variable for whole container
+var allBlocksContainer = $("#all-rows-container");
 
-// add event functionality
-// when user clicks on event block, becomes form to input new event
-// when user clicks away from event block, text stays but isn't saved yet
-// when user clicks on save, corresponding event saves to local storage
-// LOCAL STORAGE PERSISTENCE
+// variable for whole row
+var wholeRow = $("#whole-row");
 
-// user needs to be able to edit an event and save changes not just add new
+// variable for hour div
+var hourDiv = $("#hour-div");
 
+// variable for hour text
+var hourText = $("#hour-text")
+
+// variable for event div
+var eventDiv = $("#event-text-div");
+
+// variable for event text
+var eventText = $("#event-text");
+
+// variable for save button div
+var saveBtnDiv = $("#save-btn-div");
+
+// variable for save button
+var saveBtn = $("#save-btn")
+
+// array for all created events
+var eventsArray = [];
+
+// new Event Text p element
+var newEventText = document.createElement("p");
+
+// create event function
+var createEventHandler = function(){
+    newEventText.textContent = "New Text Content";
+    eventText.text(newEventText.textContent);
+};
+
+saveBtn.on("click", createEventHandler);
+
+// on blur function
+
+    // change to form (this needs to be done with event delegation)
+    // get user input and replace text in p tag when user clicks off form
+    // function to save to local storage when user clicks save button
+    // figure out how to add local storage persistence for each new task
+    
+// add styling changes if current hour is before, after, or the same as hour in p tag for each block
